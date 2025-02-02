a1355bcc476c7bb221e869f5e74c1476686818df
Kapaika <32897451+Kapaika@users.noreply.github.com>
2025-02-02 16:57:14 +0100

    feat: add components and error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Component } from 'react';
 import './App.css';
+import TopControls from './components/TopControls';
+import Results from './components/Results';
+import ErrorBoundary from './components/ErrorBoundary';
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface AppState {
+  items: [];
+  isLoading: boolean;
+  errorKey: number;
+  storedQuery: string;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+class App extends Component {
+  SWAPI_LINK = 'https://swapi.dev/api/people?search=';
+  state: AppState = {
+    items: [],
+    isLoading: false,
+    errorKey: 0,
+    storedQuery: '',
+  };
+
+  componentDidMount(): void {
+    const storedQuery = localStorage.getItem('searchQuery');
+    if (storedQuery) {
+      this.makeAPICall(storedQuery);
+      this.setState({ storedQuery: storedQuery });
+    }
+  }
+
+  makeAPICall = (name: string) => {
+    this.setState({
+      items: [],
+      isLoading: true,
+      errorKey: this.state.errorKey + 1,
+    });
+    fetch(this.SWAPI_LINK + name)
+      .then((response) => {
+        if (!response) {
+          this.setState({ items: null });
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ items: data.results, isLoading: false });
+        localStorage.setItem('searchQuery', name);
+      });
+  };
+
+  makeErrorCall = () => {
+    fetch('https://jsonplaceholder.typicode.com/invalid-endpoint')
+      .then((response) => {
+        return response.json();
+      })
+      .then(() => {
+        this.setState({ items: null });
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <TopControls
+          query={this.state.storedQuery}
+          getName={(name: string) => this.makeAPICall(name)}
+        ></TopControls>
+        {this.state.isLoading ? (
+          <h2>🌀 Loading...</h2>
+        ) : (
+          <ErrorBoundary key={this.state.errorKey}>
+            <Results items={this.state.items} />
+          </ErrorBoundary>
+        )}
+        <div className="flex justify-end mt-4">
+          <Button label="Error Button" onClick={this.makeErrorCall} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
